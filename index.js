27d6a77fd2932c1f137265ef5a218395d3bcdb27
JakkiFx <jackcielfelix555@gmail.com>
2024-01-11 22:20:07 -0300

    Passando e pegando parametros na URL

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ app.get("/blog", (req, res) => {
     res.send("Página do bloog!");
 });
 
+// Parâmetro obrigatorio
+app.get('/ola/:name/:work', (req, res) => {
+    // REQ => Dados enviados pelo usuário
+    // RES => Resposta enviada ao usuário
+    const name = req.params.name;
+    const work = req.params.work;
+
+    res.send(`<h2>Olá ${name}, você é ${work}</h2>`);
+});
+
+// Parâmetro opcional (?)
+app.get('/contato/:contact?', (req, res) => {
+
+    const param = req.params.contact;
+
+    param ? res.send(`<h3>Você passou o parâmetro ${param}</h3>`) : res.send(`<h3>Você não passou nenhum parâmetro</h3>`);
+});
+
 app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  else console.log(`Servidor rodando na porta ${PORT}`);
+  err ? console.log(err) : console.log(`Servidor rodando na porta ${PORT}`);
 });
