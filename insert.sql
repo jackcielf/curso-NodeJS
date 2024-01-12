43d282eb3b65cdd31a904d21460abdcb676fdb54
JakkiFx <jackcielfelix555@gmail.com>
2024-01-12 15:17:59 -0300

    Adicionar novo usuario na table

---
+++ insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios(nome, email, idade) VALUES('Jack', 'jack@gmail.com', 18);
+INSERT INTO usuarios(nome, email, idade) VALUES('Ana', 'ana@gmail.com', 22);
+INSERT INTO usuarios(nome, email, idade) VALUES('Pedro', 'pedro@gmail.com', 15);
+INSERT INTO usuarios(nome, email, idade) VALUES('Gabi', 'gabi@gmail.com', 8);
+INSERT INTO usuarios(nome, email, idade) VALUES('João', 'joão@gmail.com', 18);
