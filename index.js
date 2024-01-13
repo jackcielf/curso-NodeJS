63ebd98535b7bfd71caa3f598c066faac5645f3c
JakkiFx <jackcielfelix555@gmail.com>
2024-01-12 21:00:43 -0300

    Adicionando estilos a pagina

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const app = express();
 
 const PORT = 3000;
 
-app.set('view engine', 'ejs'); // Usando o 'ejs' para renderizar HTML
+app.set("view engine", "ejs"); // Usando o 'ejs' para renderizar HTML
+app.use(express.static('public')); // Para usar arquivos estaticos (css, images, ...)
 
 app.get("/", (req, res) => {
-    var nome = "Jack";
-    var lang = "TypeScript";
+  var nome = "Jack";
+  var lang = "TypeScript";
 
-    var produtos = [
-      {
-        nome: "Doritos", 
-        preco: 8.99
-      },
-      {
-        nome: "Coca-cola", 
-        preco: 5.99
-      },
-      {
-        nome: "Arroz", 
-        preco: 4.49
-      }
-    ]
+  var produtos = [
+    {
+      nome: "Doritos",
+      preco: 8.99,
+    },
+    {
+      nome: "Coca-cola",
+      preco: 5.99,
+    },
+    {
+      nome: "Arroz",
+      preco: 4.49,
+    },
+  ];
 
   res.render("index", {
     nome: nome,
     lang: lang,
     msg: true,
-    produtos: produtos
+    produtos: produtos,
   });
 });
 
